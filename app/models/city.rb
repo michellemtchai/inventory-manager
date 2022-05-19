17296af653563fad541212faf6779757f9d6efe1
michellemtchai <michelle.chai@mail.utoronto.ca>
2022-05-19 03:21:32 -0400

    add more validation for uniqueness

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ApplicationRecord
   validates :name, :presence => true
   validates :lat, :presence => true
   validates :long, :presence => true
-  validates_uniqueness_of :country_id, :scope => [:name, :region]
+  validates_uniqueness_of :name, :scope => [:name, :country_id]
 
   def label
     if region.nil?
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :inventory_items
   validates :name, :presence => true
   validates :description, :presence => true
+  validates_uniqueness_of :name, :scope => [:description]
 
   def name_with_id
     "#{name} (ID: #{id})"
